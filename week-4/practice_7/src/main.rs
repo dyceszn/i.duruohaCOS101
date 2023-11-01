ac95664a6440d1de3394d2b65bff3729d6968c21
dyceszn <147930425+dyceszn@users.noreply.github.com>
2023-11-01 16:52:05 +0100

    Add files via upload

---
+++ week-4/practice_7/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let k1 = "Yemisi".to_string();
+    let k2 = "Shyllom".to_string();
+    let k3 = "Museum".to_string();
+    let k4 = "of".to_string();
+    let k5 = "Art".to_string();
+    let k6 = "PAU".to_string();
+
+    //format score
+    let k7 = format!("{} {} {} {} {} {}",k1,k2,k3,k4,k5,k6);
+
+
+    //print output
+    println!("\n {}",k7);
+}
