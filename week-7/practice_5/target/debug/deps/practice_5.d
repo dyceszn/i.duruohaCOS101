c379864ecb34b2da1797be1f01d17c2c1b54b071
dyceszn <147930425+dyceszn@users.noreply.github.com>
2023-11-23 14:19:39 +0100

    Add files via upload

---
+++ week-7/practice_5/target/debug/deps/practice_5.d
@@ -0,0 +1,5 @@
+C:\Users\duruo\Documents\i.duruohaCOS101\week-7\practice_5\target\debug\deps\practice_5.exe: src\main.rs
+
+C:\Users\duruo\Documents\i.duruohaCOS101\week-7\practice_5\target\debug\deps\practice_5.d: src\main.rs
+
+src\main.rs:
---
+++ week-7/practice_5/target/debug/practice_5.d
@@ -0,0 +1,1 @@
+C:\Users\duruo\Documents\i.duruohaCOS101\week-7\practice_5\target\debug\practice_5.exe: C:\Users\duruo\Documents\i.duruohaCOS101\week-7\practice_5\src\main.rs
