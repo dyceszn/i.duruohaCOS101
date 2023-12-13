d3b85549d4c50d10e5ee92b51ae6cd696c5e3742
dyceszn <147930425+dyceszn@users.noreply.github.com>
2023-12-13 16:40:50 +0100

    My tenth commit

---
+++ week-10/practice-3/target/debug/deps/practice_3.d
@@ -0,0 +1,5 @@
+C:\Users\duruo\Documents\School\i.duruohaCOS101\week-10\practice-3\target\debug\deps\practice_3.exe: src\main.rs
+
+C:\Users\duruo\Documents\School\i.duruohaCOS101\week-10\practice-3\target\debug\deps\practice_3.d: src\main.rs
+
+src\main.rs:
---
+++ week-10/practice-4/target/debug/deps/practice_4.d
@@ -0,0 +1,5 @@
+C:\Users\duruo\Documents\School\i.duruohaCOS101\week-10\practice-4\target\debug\deps\practice_4.exe: src\main.rs
+
+C:\Users\duruo\Documents\School\i.duruohaCOS101\week-10\practice-4\target\debug\deps\practice_4.d: src\main.rs
+
+src\main.rs:
