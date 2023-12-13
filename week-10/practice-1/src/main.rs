6e6df4899a1e5c547ae3695b61b9cf52ad03181d
dyceszn <147930425+dyceszn@users.noreply.github.com>
2023-12-13 16:39:30 +0100

    My tenth commit

---
+++ week-10/practice-1/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    
+let v = vec![101, 250,130, 400];
+
+let v2 = v;
+
+println!("{:?}",v );
+
+}
---
+++ week-10/practice-1/target/debug/deps/practice_1.d
@@ -0,0 +1,5 @@
+C:\Users\duruo\Documents\School\i.duruohaCOS101\week-10\practice-1\target\debug\deps\practice_1.exe: src\main.rs
+
+C:\Users\duruo\Documents\School\i.duruohaCOS101\week-10\practice-1\target\debug\deps\practice_1.d: src\main.rs
+
+src\main.rs:
